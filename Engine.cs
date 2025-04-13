0737cde75b5fa605d5470c9445f34f96d77d615d
Taka005 <contact@takadev.jp>
2025-04-13 11:03:21 +0900

    .editorconfigの設定追加とEngineクラスの改修
    .editorconfigファイルにC#のコーディング規則やスタイル、命名規則に関する詳細な設定を追加し、プロジェクト全体で一貫したコーディングスタイルを適用できるようにしました。
    
    Engine.csファイルでは、クラスの構造を大幅に変更し、コンストラクタの引数を追加しました。プロパティの設定方法をメソッドを介して行うように変更し、エンジンの開始と停止のロジックを実装するためにstartおよびstopメソッドを追加しました。これにより、エンジンの状態管理が強化され、エラー処理も改善されました。

--- Engine.cs
+++ Engine.cs
@@ -1,48 +1,74 @@
 ﻿using System.Threading;
 
+namespace PhysicsEngineCore{
+    public class Engine{
+        private int pps;
+        private bool isStarted = false;
+        private bool isTrackingMode = false;
+        private bool isDebugMode = false;
+        private Timer loopTimer;
+        private float playBackSpeed;
+        private float trackingInterval;
+        private int trackingCount = 0;
+        private int trackingLimit;
 
-namespace PhysicsEngineCore {
-    public class Engine(float playBackSpeed, float trackingInterval, int trackingLimit){
-        private float _playBackSpeed = checkPlayBackSpeedValue(playBackSpeed);
-        private float _trackingInterval = checkTrackingIntervalValue(trackingInterval);
-        private int _trackingLimit = checkTrackingLimitValue(trackingLimit);
+        public Engine(int pps, float playBackSpeed, float trackingInterval, int trackingLimit){
+            this.pps = pps;
+            this.playBackSpeed = checkPlayBackSpeedValue(playBackSpeed);
+            this.trackingInterval = checkTrackingIntervalValue(trackingInterval);
+            this.trackingLimit = checkTrackingLimitValue(trackingLimit);
+            this.loopTimer = new Timer(this.loop!, null, 0, (int)((1000 / this.pps) / this.playBackSpeed));
+        }
 
-        public float playBackSpeed{
-            get{
-                return _playBackSpeed;
-            }
-            set{
-                _playBackSpeed = checkPlayBackSpeedValue(value);
+        public void setPlayBackSpeed(float value){
+            this.playBackSpeed = checkPlayBackSpeedValue(value);
+
+            if(this.isStarted){
+                this.loopTimer.Change(0, (int)((1000 / this.pps) / this.playBackSpeed));
             }
         }
 
-        public float trackingInterval{
-            get{
-                return _trackingInterval;
-            }
-            set{
-                _trackingInterval = checkTrackingIntervalValue(value);
-            }
+        public void setTrackingInterval(float value){
+            this.trackingInterval = checkTrackingIntervalValue(value);
         }
-        public int trackingLimit{
-            get{
-                return _trackingLimit;
-            }
-            set{
-                _trackingLimit = checkTrackingLimitValue(value);
-            }
+
+        public void setTrackingLimit(int value){
+            this.trackingLimit = checkTrackingLimitValue(value);
         }
+
+        public void start(){
+            if(this.isStarted) throw new Exception("既にシステムは開始されています");
+
+            this.isStarted = true;
+            this.loopTimer.Change(0, (int)((1000 / this.pps) / this.playBackSpeed));
+        }
+
+        public void stop(){
+            if(!this.isStarted) throw new Exception("既にシステムは停止しています");
+
+            this.isStarted = false;
+            this.trackingCount = 0;
+            this.loopTimer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private void loop(Object state){
+
+        }
+
+        
 
         private static float checkPlayBackSpeedValue(float playBackSpeed){
             if(playBackSpeed < 0) throw new Exception("再生速度(playBackSpeed)は0以上に設定する必要があります");
 
             return playBackSpeed;
         }
+
         private static float checkTrackingIntervalValue(float trackingInterval){
             if(trackingInterval < 0) throw new Exception("トラッキング間隔(trackingInterval)は0以上に設定する必要があります");
 
             return trackingInterval;
         }
+
         private static int checkTrackingLimitValue(int trackingLimit){
             if(trackingLimit < 0) throw new Exception("トラッキング数(trackingLimit)は0以上に設定する必要があります");
 
