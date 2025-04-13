b082d2dc075399a158d3886f6045b25d02f523b2
Taka005 <contact@takadev.jp>
2025-04-13 13:12:08 +0900

    回転強度の追加と位置解決の修正
    `Core.cs`ファイルにおいて、以下の変更を行いました:
    - 新しい定数`ROTATION_STRENGTH`を追加し、値を`50`に設定。
    - `solvePosition`メソッド内で`difference`の計算を修正。
    - 物体の位置修正に加え、回転に関連する計算を追加し、`rotateSpeed`を条件に応じて調整。

--- Core.cs
+++ Core.cs
@@ -4,12 +4,13 @@ using PhysicsEngineCore.Objects;
 namespace PhysicsEngineCore{
     class Core{
         private static readonly float CORRECTION_NUMBER = 0.000001f;
+        private static readonly float ROTATION_STRENGTH = 50;
 
         protected void solvePosition(Entity source, Entity target){
             float totalMass = source.invMass + target.invMass;
             if(totalMass == 0) return;
 
-            Vector2 difference = source.position - target.position;
+            Vector2 difference = target.position - source.position;
 
             if(
                 Math.Abs(difference.X) >= source.radius + target.radius ||
@@ -24,6 +25,18 @@ namespace PhysicsEngineCore{
 
             source.position += correction * source.invMass;
             target.position -= correction * target.invMass;
+
+            float angle = -difference.X * source.velocity.Y + difference.X * source.velocity.X;
+
+            float rotate = (float)(Math.Acos(Vector2.Dot(difference,source.velocity) / (distance * source.velocity.Length())) * (180 / Math.PI));
+
+            if(angle > 0){
+                source.rotateSpeed -= rotate / ROTATION_STRENGTH;
+                target.rotateSpeed += rotate / ROTATION_STRENGTH;
+            }else if(angle < 0){
+                source.rotateSpeed += rotate / ROTATION_STRENGTH;
+                target.rotateSpeed -= rotate / ROTATION_STRENGTH;
+            }
         }
     }
 }
