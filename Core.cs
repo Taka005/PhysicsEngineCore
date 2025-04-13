82203569d61672397dfb61c48a4bc453b55dbc4d
Taka005 <contact@takadev.jp>
2025-04-13 12:07:31 +0900

    物理エンジンの位置解決ロジックを追加
    `System.Numerics`と`PhysicsEngineCore.Objects`の名前空間をインポートし、`Core`クラスに`solvePosition`メソッドを追加しました。このメソッドは、2つのエンティティの質量、位置の差、半径を考慮して位置を修正します。エンティティ間の距離が半径の合計を超えている場合や、質量がゼロの場合には何も行わず、そうでない場合には位置を調整します。これにより、物理エンジンの動作が改善されることが期待されます。

--- Core.cs
+++ Core.cs
@@ -1,5 +1,29 @@
-﻿namespace PhysicsEngineCore{
-    class Core{
+﻿using System.Numerics;
+using PhysicsEngineCore.Objects;
 
+namespace PhysicsEngineCore{
+    class Core{
+        private static readonly float CORRECTION_NUMBER = 0.000001f;
+
+        protected void solvePosition(Entity source, Entity target){
+            float totalMass = source.invMass + target.invMass;
+            if(totalMass == 0) return;
+
+            Vector2 difference = source.position - target.position;
+
+            if(
+                Math.Abs(difference.X) >= source.radius + target.radius ||
+                Math.Abs(difference.Y) >= source.radius + target.radius
+            ) return;
+
+            float distance = difference.Length();
+            if(distance > source.radius + target.radius) return;
+
+            float move = (distance - (source.radius + target.radius)) / (distance * totalMass + CORRECTION_NUMBER) * source.stiffness;
+            Vector2 correction = difference * move;
+
+            source.position += correction * source.invMass;
+            target.position -= correction * target.invMass;
+        }
     }
 }
