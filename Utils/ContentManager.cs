035ec95590a9b8166f4eac1f9d5dc2b111104cf1
Taka005 <contact@takadev.jp>
2025-05-19 14:44:08 +0900

    ContentManagerクラスの追加と管理機能の実装
    `ContentManager.cs` に物体と地面を管理する `ContentManager` クラスを追加しました。このクラスは、物体と地面のリストを保持し、追加や削除のコマンドをキューに追加するメソッドを提供します。また、キューに基づいてオブジェクトとグラウンドを同期する `Sync` メソッドも実装されています。さらに、コマンドの種類を表す `CommandType` 列挙型と、物体および地面のキューを表す `QueueObject` と `QueueGround` クラスも追加されました。

---
+++ Utils/ContentManager.cs
@@ -0,0 +1,83 @@
+﻿using PhysicsEngineCore.Objects;
+
+namespace PhysicsEngineCore.Utils{
+    class ContentManager{
+        private readonly List<IObject> objects = [];
+        private readonly List<IGround> grounds = [];
+        private readonly List<QueueObject> queueObjects = [];
+        private readonly List<QueueGround> queueGrounds = [];
+
+        public void AddObject(IObject target){
+            QueueObject queue = new QueueObject{
+                command = CommandType.Add,
+                target = target
+            };
+
+            this.queueObjects.Add(queue);
+        }
+
+        public void RemoveObject(IObject target){
+            QueueObject queue = new QueueObject{
+                command = CommandType.Remove,
+                target = target
+            };
+
+            this.queueObjects.Add(queue);
+        }
+
+        public void AddGround(IGround target){
+            QueueGround queue = new QueueGround {
+                command = CommandType.Add,
+                target = target
+            };
+
+            this.queueGrounds.Add(queue);
+        }
+
+        public void RemoveGround(IGround target){
+            QueueGround queue = new QueueGround{
+                command = CommandType.Add,
+                target = target
+            };
+
+            this.queueGrounds.Add(queue);
+        }
+
+        public void Sync(){
+            this.queueObjects.ForEach(obj =>{
+                if(obj.target == null) return;
+
+                if(obj.command == CommandType.Add){
+                    this.objects.Add(obj.target);
+                }else if(obj.command == CommandType.Remove){
+                    this.objects.Remove(obj.target);
+                }
+            });
+
+            this.queueGrounds.ForEach(obj=>{
+                if(obj.target == null) return;
+
+                if(obj.command == CommandType.Add){
+                    this.grounds.Add(obj.target);
+                }else if(obj.command == CommandType.Remove){
+                    this.grounds.Remove(obj.target);
+                }
+            });
+        }
+    }
+
+    class QueueObject{
+        public CommandType command;
+        public IObject? target;
+    }
+
+    class QueueGround{
+        public CommandType command;
+        public IGround? target;
+    }
+
+    enum CommandType{
+        Add = 0,
+        Remove = 1
+    }
+}
