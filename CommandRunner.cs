8d16b774ab9ce38ce780a566cdc205eb5bc6ba30
Taka005 <contact@takadev.jp>
2025-08-12 19:24:46 +0900

    調整

--- CommandRunner.cs
+++ CommandRunner.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 namespace PhysicsEngineCore{
     class CommandRunner(Engine engine){
@@ -10,19 +6,44 @@ namespace PhysicsEngineCore{
 
         private readonly Dictionary<string, object> globalVariables = [];
 
-        public void Execute(string command) {
+        public void Execute(string command,Dictionary<string, object> localVariables) {
             string[] parts = command.Split(" ");
             string commandName = parts[0];
 
-            if(commandName == "/fill") {
-
+            if(commandName == "/set") {
+                this.HandleSetCommand(parts.Skip(1).ToArray(),localVariables);
             } else {
                 throw new Exception($"不明なコマンド: {commandName}");
             }
         }
 
-        private void HandleFillCommand(string[] commandParts) {
-        
+        private void HandleSetCommand(string[] args, Dictionary<string, object> localVariables) {
+            if(args.Length < 2) throw new Exception("Setコマンドの引数の数が正しくありません。引数は二つである必要があります");
+
+            string varName = args[0];
+            string value = args[1];
+            bool isGlobal = args.Length > 2 && args[2].ToLower() == "global";
+
+            string[] parts = varName.Split(":");
+
+            if(parts.Length == 2) {
+            
+            }else if(parts.Length == 1) {
+                if(isGlobal) {
+                    globalVariables[varName] = value;
+                } else {
+                    localVariables[varName] = value;
+                }
+            } else {
+                throw new Exception("変数名の形式が正しくありません。varName:propertyNameである必要があります");
+            }
+        }
+
+        private object? GetObjectProperty(object obj, string propName) {
+            PropertyInfo? prop = obj.GetType().GetProperty(propName);
+            if(prop == null) throw new Exception($"プロパティ '{propName}' がオブジェクト '{obj.GetType().Name}' に存在しません");
+
+            return prop.GetValue(obj);
         }
     }
 }
