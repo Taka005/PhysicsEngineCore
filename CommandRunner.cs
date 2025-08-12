fd4e20f00bacf2106a75e0bb106f01af831400d4
Taka005 <contact@takadev.jp>
2025-08-12 17:17:12 +0900

    add command

---
+++ CommandRunner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsEngineCore{
+    class CommandRunner(Engine engine){
+        private readonly Engine engine = engine;
+
+        private readonly Dictionary<string, object> globalVariables = [];
+
+        public void Execute(string command) {
+            string[] parts = command.Split(" ");
+            string commandName = parts[0];
+
+            if(commandName == "/fill") {
+
+            } else {
+                throw new Exception($"不明なコマンド: {commandName}");
+            }
+        }
+
+        private void HandleFillCommand(string[] commandParts) {
+        
+        }
+    }
+}
