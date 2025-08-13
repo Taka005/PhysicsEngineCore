a52647ec147c0f73ed077a5b42dc1aa0146bc070
Taka005 <contact@takadev.jp>
2025-08-13 09:06:19 +0900

    調整

--- CommandRunner.cs
+++ CommandRunner.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Reflection;
 
 namespace PhysicsEngineCore{
     class CommandRunner(Engine engine){
@@ -11,32 +12,47 @@ namespace PhysicsEngineCore{
             string commandName = parts[0];
 
             if(commandName == "/set") {
-                this.HandleSetCommand(parts.Skip(1).ToArray(),localVariables);
+                this.HandleSetCommand(parts.Skip(1).ToArray(), localVariables);
+            }else if(commandName == "/get") {
+
             } else {
                 throw new Exception($"不明なコマンド: {commandName}");
             }
         }
 
         private void HandleSetCommand(string[] args, Dictionary<string, object> localVariables) {
-            if(args.Length < 2) throw new Exception("Setコマンドの引数の数が正しくありません。引数は二つである必要があります");
+            if(args.Length < 2) throw new Exception("Setコマンドの引数の数が正しくありません。引数は2つである必要があります");
 
             string varName = args[0];
-            string value = args[1];
+            object? value = this.SolveVariable(args[1], localVariables);
             bool isGlobal = args.Length > 2 && args[2].ToLower() == "global";
 
-            string[] parts = varName.Split(":");
+            if(value == null) throw new Exception($"' {args[1]} ' を解決できませんでした");
 
-            if(parts.Length == 2) {
-            
-            }else if(parts.Length == 1) {
-                if(isGlobal) {
-                    globalVariables[varName] = value;
-                } else {
-                    localVariables[varName] = value;
-                }
+            if(isGlobal) {
+                this.globalVariables[varName] = value;
             } else {
-                throw new Exception("変数名の形式が正しくありません。varName:propertyNameである必要があります");
+                localVariables[varName] = value;
             }
+        }
+
+        private void HandleGetCommand(string[] args, Dictionary<string, object> localVariables) {
+
+        }
+
+        private object? SolveVariable(string varName, Dictionary<string, object> localVariables) {
+            string[] parts = varName.Split(":");
+            Object? value;
+
+            if(localVariables.TryGetValue(parts[0], out object? localValue)) {
+                value = localValue;
+            }else if(this.globalVariables.TryGetValue(parts[0], out object? globalValue)) {
+                value = globalValue;
+            } else {
+                value = varName;
+            }
+
+            return parts.Length > 1 ? this.GetObjectProperty(value, parts[1]) : value;
         }
 
         private object? GetObjectProperty(object obj, string propName) {
