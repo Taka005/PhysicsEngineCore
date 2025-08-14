22c6dd99be1cd9c119be5d7461c2cf73c5bec15e
Taka005 <contact@takadev.jp>
2025-08-14 16:38:51 +0900

    調整

--- CommandRunner.cs
+++ CommandRunner.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.Eventing.Reader;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace PhysicsEngineCore{
     class CommandRunner(Engine engine){
@@ -12,9 +11,11 @@ namespace PhysicsEngineCore{
             string commandName = parts[0];
 
             if(commandName == "/set") {
-                this.HandleSetCommand(parts.Skip(1).ToArray(), localVariables);
+                this.HandleSetCommand([.. parts.Skip(1)], localVariables);
             }else if(commandName == "/get") {
-
+                this.HandleGetCommand([.. parts.Skip(1)], localVariables);
+            } else if(commandName == "/update") {
+                this.HandleUpdateCommand([.. parts.Skip(1)], localVariables);
             } else {
                 throw new Exception($"不明なコマンド: {commandName}");
             }
@@ -37,22 +38,42 @@ namespace PhysicsEngineCore{
         }
 
         private void HandleGetCommand(string[] args, Dictionary<string, object> localVariables) {
+            if(args.Length < 2) throw new Exception("Getコマンドの引数の数が正しくありません。引数は2つである必要があります");
+
+            string varName = args[0];
+            object? value = this.GetAnyObject(args[1]);
+            bool isGlobal = args.Length > 2 && args[2].ToLower() == "global";
+
+            if(value == null) throw new Exception($"' {args[1]} ' を解決できませんでした");
+
+            if(isGlobal) {
+                this.globalVariables[varName] = value;
+            } else {
+                localVariables[varName] = value;
+            }
+        }
+
+        private void HandleUpdateCommand(string[] args, Dictionary<string, object> localVariables) {
 
         }
 
         private object? SolveVariable(string varName, Dictionary<string, object> localVariables) {
-            string[] parts = varName.Split(":");
-            Object? value;
+            if(varName.StartsWith("\"") && varName.EndsWith("\"")) {
+                return varName[1..^1];
+            }{
+                string[] parts = varName.Split(":");
+                Object? value;
 
-            if(localVariables.TryGetValue(parts[0], out object? localValue)) {
-                value = localValue;
-            }else if(this.globalVariables.TryGetValue(parts[0], out object? globalValue)) {
-                value = globalValue;
-            } else {
-                value = varName;
+                if(localVariables.TryGetValue(parts[0], out object? localValue)) {
+                    value = localValue;
+                } else if(this.globalVariables.TryGetValue(parts[0], out object? globalValue)) {
+                    value = globalValue;
+                } else {
+                    throw new Exception($"変数 '{varName}' が存在しませんでした");
+                }
+
+                return parts.Length > 1 ? this.GetObjectProperty(value, parts[1]) : value;
             }
-
-            return parts.Length > 1 ? this.GetObjectProperty(value, parts[1]) : value;
         }
 
         private object? GetObjectProperty(object obj, string propName) {
@@ -60,6 +81,18 @@ namespace PhysicsEngineCore{
             if(prop == null) throw new Exception($"プロパティ '{propName}' がオブジェクト '{obj.GetType().Name}' に存在しません");
 
             return prop.GetValue(obj);
+        }
+
+        private object? GetAnyObject(string id) {
+            object? obj = this.engine.GetObject(id);
+            object? ground = this.engine.GetGround(id);
+            object? effect = this.engine.GetEffect(id);
+
+            if(obj != null) return obj;
+            if(ground != null) return ground;
+            if(effect != null) return effect;
+
+            return null;
         }
     }
 }
